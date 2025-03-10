6280a6f39fbfe6cbf0869096666376382406e129
lqt0223 <lqt19920223@126.com>
2025-03-10 10:19:54 +0800

    try use server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import BBSCardList from "./components/BBSCardList";
 
 export default async function Home() {
+  "use server"
+
   const list = await prisma.post.findMany()
 
   return <main>
